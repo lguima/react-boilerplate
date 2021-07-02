47c46846210b152232df5b1995cd3f4e7d0ef2af
Lucas Guima <lucasguima.felipe@gmail.com>
2021-07-02 16:36:52 -0300

    Update Jest coverage directory config

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,6 @@ module.exports = {
   testEnvironment: 'jsdom',
   testPathIgnorePatterns: ['/node_modules/', '/.next/'],
   collectCoverage: true,
-  collectCoverageFrom: ['src/**/*.tsx?'],
+  collectCoverageFrom: ['src/**/*.ts(x)'],
   setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts']
 }
