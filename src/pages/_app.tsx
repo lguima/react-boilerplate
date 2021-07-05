f99ae50574633c7953112c78050727f9936ba58b
Lucas Guima <lucasguima.felipe@gmail.com>
2021-07-05 14:05:46 -0300

    Apply the global style

---
+++ src/pages/_app.tsx
@@ -0,0 +1,14 @@
+import type { AppProps } from 'next/app'
+
+import GlobalStyle from 'styles/global'
+
+function App({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <GlobalStyle />
+      <Component {...pageProps} />
+    </>
+  )
+}
+
+export default App
