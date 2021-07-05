5210e1c659ceecef1d6c906637478c75445b9034
Lucas Guima <lucasguima.felipe@gmail.com>
2021-07-05 14:15:08 -0300

    Apply some meta information to the app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,20 @@
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
 
 import GlobalStyle from 'styles/global'
 
 function App({ Component, pageProps }: AppProps) {
   return (
     <>
+      <Head>
+        <title>React - Boilerplate</title>
+        <link rel="shorcut icon" href="img/icon-512.png" />
+        <link rel="apple-touch-icon" href="img/icon-512.png" />
+        <meta
+          name="description"
+          content="A simple project starter to work with React, TypeScript, Next.js and Styled Components."
+        />
+      </Head>
       <GlobalStyle />
       <Component {...pageProps} />
     </>
