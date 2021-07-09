7708f81b49849c0a12b90e697fdd5b655dbcf4d2
Lucas Guima <lucasguima.felipe@gmail.com>
2021-07-09 20:35:17 -0300

    Replace the global style file

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,7 @@
 import type { AppProps } from 'next/app'
 import Head from 'next/head'
 
-import GlobalStyle from 'styles/global'
+import GlobalStyle from 'assets/styles/GlobalStyle'
 
 function App({ Component, pageProps }: AppProps) {
   return (
