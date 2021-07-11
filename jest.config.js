64d009c4c85ff86f2ccc4450aa1a644021fa8b94
Lucas Guima <lucasguima.felipe@gmail.com>
2021-07-11 14:13:34 -0300

    Add files .tsx to tests coverages
    Remove story files from test coverage

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,9 @@ module.exports = {
   testEnvironment: 'jsdom',
   testPathIgnorePatterns: ['/node_modules/', '/.next/'],
   collectCoverage: true,
-  collectCoverageFrom: ['src/**/*.ts(x)'],
+  collectCoverageFrom: [
+    'src/**/*.(ts|tsx)',
+    '!src/components/**/*.stories.(js|jsx|ts|tsx)'
+  ],
   setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts']
 }
