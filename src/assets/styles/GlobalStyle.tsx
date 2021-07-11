a5702a94d50c1dfa20b9bc6e1f0bbb52ad6446c4
Lucas Guima <lucasguima.felipe@gmail.com>
2021-07-11 10:04:15 -0300

    Add Material UI default font

--- src/assets/styles/GlobalStyle.tsx
+++ src/assets/styles/GlobalStyle.tsx
@@ -1,6 +1,18 @@
 import React from 'react'
-import { GlobalStyles } from 'twin.macro'
+import tw, { GlobalStyles as BaseStyle } from 'twin.macro'
+import { createGlobalStyle } from 'styled-components'
 
-const GlobalStyle = () => <GlobalStyles />
+const CustomStyle = createGlobalStyle`
+  html, body, #__next {
+    ${tw`h-full`}
+  }
+`
+
+const GlobalStyle = () => (
+  <>
+    <BaseStyle />
+    <CustomStyle />
+  </>
+)
 
 export default GlobalStyle
