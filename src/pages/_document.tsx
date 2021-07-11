ebc6e1721890b828b7d6b0b41fc6c96cf534c3e0
Lucas Guima <lucasguima.felipe@gmail.com>
2021-07-11 13:23:47 -0300

    Add the PWA manifest to the application

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -38,6 +38,7 @@ export default class MyDocument extends Document {
     return (
       <Html lang="pt-BR">
         <Head>
+          <link rel="manifest" href="/manifest.json" />
           <link
             rel="stylesheet"
             href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
