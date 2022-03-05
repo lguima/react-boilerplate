94311a02b695b94cb2fe10a90796bb9deaa32f95
Lucas Guima <lucasguima.felipe@gmail.com>
2022-03-05 00:30:56 -0300

    Configure Storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,10 +1,21 @@
 module.exports = {
-  "stories": [
-    "../src/components/**/*.stories.mdx",
+  staticDirs: ['../public'],
+  stories: [
     "../src/components/**/*.stories.@(js|jsx|ts|tsx)"
   ],
-  "addons": [
+  addons: [
     "@storybook/addon-essentials",
     "storybook-addon-next-router"
-  ]
+  ],
+  babel: async (options) => ({
+    ...options,
+    plugins: [...options.plugins, '@babel/plugin-transform-react-jsx']
+  }),
+  webpackFinal: (config) => {
+    config.resolve.modules.push(`${process.cwd()}/src`)
+    return config
+  },
+  core: {
+    builder: 'webpack5'
+  }
 }
